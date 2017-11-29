5e7401b927730e41d8c6e3d465416a25792927bd
Richard Hoffman <rhoffman219@gmail.com>
2017-11-29 16:55:07 -0600

    entered code for search controller and search view to display search results

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,25 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+
+            List<Dictionary<string, string>> newJobs = new List<Dictionary<string, string>>();
+
+            ViewBag.columns = ListController.columnChoices;
+            if (searchType != "all")
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            else
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            }
+
+            ViewBag.newJobs = ViewBag.jobs;
+            return View();
+
+        }
     }
 }
